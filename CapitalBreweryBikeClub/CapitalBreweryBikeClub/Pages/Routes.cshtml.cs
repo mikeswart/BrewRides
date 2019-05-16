a7500cb9d9f49c03f0333f76370499d3b210cc7c
Mike Swart <mike.swart@gmail.com>
2019-05-15 20:48:47 -0500

    Routing

---
+++ CapitalBreweryBikeClub/CapitalBreweryBikeClub/Pages/Routes.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CapitalBreweryBikeClub.Pages
+{
+    public class routeModel : PageModel
+    {
+        public string Route { get; set; }
+
+        public void OnGet(string route)
+        {
+            if (route.Equals("list", StringComparison.InvariantCultureIgnoreCase))
+            {
+                // render list
+            }
+
+            Route = route;
+        }
+    }
+}
